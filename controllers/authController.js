a6fc9632a7c76ca83119ecae62aa41743b1f4930
Hieu Nguyen <hieunguyen6524@gmail.com>
2025-08-22 20:44:08 +0700

    1

--- controllers/authController.js
+++ controllers/authController.js
@@ -19,6 +19,7 @@ const createSendToken = (user, statusCode, res) => {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000,
     ),
+    sameSite: 'none',
   };
 
   if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
